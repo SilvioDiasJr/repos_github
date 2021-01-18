b00cee13a4dbbfdad02d96e09af982ad70965aee
Silvio <silvio.dias@etec.sp.gov.br>
2021-01-18 02:04:02 -0300

    criar o componente Repositorio

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-// import { Container } from './styles';
+import api from '../../services/api'
 
-function Repositorio() {
-  return <div />;
+import { Container } from './styles';
+
+function Repositorio({match}) {
+  const [repositorio, setRepositorio] = useState({})
+  const [issues, setIssues] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function load(){
+      // Fazer a decodificação do parametro enviado pela url
+      const nameRepo = decodeURIComponent(match.params.repositorio)
+
+      // Fazer requisições simultaneas, salvar em um array e descontruir
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nameRepo}`),
+        api.get(`repos/${nameRepo}/issues`, {
+          params:{
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ])
+      setRepositorio(repositorioData.data)
+      setIssues(issuesData.data)
+      setLoading(false)
+    }
+
+    load()
+  },[match.params.repositorio])
+  return (
+    <Container>
+
+    </Container>
+  )
 }
 
 export default Repositorio;
